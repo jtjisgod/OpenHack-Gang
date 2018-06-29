2e82d7992a577accf345c3af4aea7e48ba466d63
scio <ghsehr1@gmail.com>
2018-06-29 17:30:43 +0900

    modify music downloader

--- Server/AI/music_downloader.py
+++ Server/AI/music_downloader.py
@@ -17,11 +17,14 @@ for i in range(len(data)):
 pd_data=pd.DataFrame(data)
 pd_data.columns = ['index','genre','artist','playback','track_num','bgimage','title','ZeroCrossing Rate','Energy','EntropyofEnergy','SpectralCentroid','SpectralSpread','SpectralEntropy','SpectralFlux','Spectral Rolloff','MFCCs','ChromaVector','ChromaDeviation','f12']
 
+#hip list filtering
+main_data=pd_data[pd_data.playback<20000]
+
 #np setting
 np_data=pd_data.as_matrix()
 
 # music file download at soundcloud
-for d in data:
+for d in main_data:
     track_num=str(d[4]) 
     url='http://api.soundcloud.com/tracks/'+track_num+'/download?client_id=unnFdubicpq7RVFFsQucZzduDPQTaCYy'
     path='~/Desktop/2018_openHack/music/'+str(d[2])+'_'+str(d[6])+'_'+str(d[4])+'.mp3'
