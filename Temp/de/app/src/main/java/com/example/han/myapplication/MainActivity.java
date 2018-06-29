586c6640046d41972cd1264123fb55f181d583cf
hanseonghye <hans12312@naver.com>
2018-06-29 23:50:32 +0900

    to circle2

--- Temp/de/app/src/main/java/com/example/han/myapplication/MainActivity.java
+++ Temp/de/app/src/main/java/com/example/han/myapplication/MainActivity.java
@@ -2,6 +2,11 @@ package com.example.han.myapplication;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -14,8 +19,6 @@ import java.io.InputStream;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
-
     ArrayList<ImageView> musicImage = new ArrayList<>();
 
     @Override
@@ -23,31 +26,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        musicImage.add((ImageView)findViewById(R.id.lp1));
-        musicImage.add((ImageView)findViewById(R.id.lp2));
-        musicImage.add((ImageView)findViewById(R.id.lp3));
-        musicImage.add((ImageView)findViewById(R.id.lp4));
-        musicImage.add((ImageView)findViewById(R.id.lp5));
         musicImage.add((ImageView)findViewById(R.id.lp6));
+        musicImage.add((ImageView)findViewById(R.id.lp5));
+        musicImage.add((ImageView)findViewById(R.id.lp4));
+        musicImage.add((ImageView)findViewById(R.id.lp3));
+        musicImage.add((ImageView)findViewById(R.id.lp2));
+        musicImage.add((ImageView)findViewById(R.id.lp1));
 
 
         musicImage.get(0).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                loadImages();
+                musicImage.get(0).setImageResource(R.mipmap.ic_ab2);
+
             }
         });
 
     }
-
     private void loadImages() {
         (new DownloadImageTask(musicImage.get(0))).execute("https://i2.wp.com/beebom.com/wp-content/uploads/2016/01/Reverse-Image-Search-Engines-Apps-And-Its-Uses-2016.jpg");
     }
+
 
     private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
         ImageView bmImage;
 
         public DownloadImageTask(ImageView bmImage) {
+
+
             this.bmImage = bmImage;
         }
 
@@ -62,6 +68,14 @@ public class MainActivity extends AppCompatActivity {
                 Log.e("Error", e.getMessage());
                 e.printStackTrace();
             }
+
+            Canvas canvas = new Canvas(mIcon11);
+            Paint paint=new Paint();
+            Rect rect=new Rect(0,0,mIcon11.getWidth(),mIcon11.getHeight());
+            paint.setAntiAlias(true);
+            canvas.drawCircle(mIcon11.getWidth()/2,mIcon11.getHeight()/2,mIcon11.getWidth()/2,paint);
+            paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+            canvas.drawBitmap(mIcon11,rect,rect,paint);
             return mIcon11;
         }
 
